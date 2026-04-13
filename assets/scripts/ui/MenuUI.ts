159a65c824e8a89bbd55a07d18674065abc9cc7a
NguyenHuuNam3 <nhnam4411@gmail.com>
2026-04-13 22:09:13 +0700

    login UI

---
+++ assets/scripts/ui/MenuUI.ts
@@ -0,0 +1,9 @@
+import { _decorator, Component } from "cc";
+const { ccclass, property } = _decorator;
+
+@ccclass("MenuUI")
+export class MenuUI extends Component {
+  start() {}
+
+  update(deltaTime: number) {}
+}
